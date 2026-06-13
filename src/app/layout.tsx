8f21194c6a69d276719aab47a3d077cc069103e7
Dhrubo Talukder <dhrubot1442@gmail.com>
2026-06-13 08:03:30 +0600

    Update mobile view

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -22,8 +22,8 @@ export default function RootLayout({
   children,
 }: Readonly<{ children: React.ReactNode }>) {
   return (
-    <html lang="en" className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}>
-      <body className="min-h-full flex flex-col">{children}</body>
+   <html lang="en" className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`} style={{ backgroundColor: "#0A2540" }}>
+      <body className="min-h-full flex flex-col" style={{ backgroundColor: "#0A2540" }}>{children}</body>
     </html>
   );
 }
